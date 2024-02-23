95060f1cacd784cfcb0ac695bc6e837fe36dade8
aakritimahaldar <123234867+aakritimahaldar@users.noreply.github.com>
2024-02-23 13:20:56 +0530

    Add files via upload

---
+++ TopBar.js
@@ -0,0 +1,11 @@
+const TopBar = () => {
+    return (
+        <div className="top__bar">
+            <h3>
+                React Audio Player is
+            </h3>
+        </div>
+    );
+};
+
+export default TopBar;
